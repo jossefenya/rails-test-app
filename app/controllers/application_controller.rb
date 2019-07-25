38e7578e50b90edeb026353187365b98392a328e
Sergei <sergeishekotihin@gmail.com>
2019-07-25 11:11:04 +0300

    new controller replace root path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
+# Root of project
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hello
+    render plain: 'Hello, Rails!'
+  end
 end
